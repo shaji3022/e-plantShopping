c0e12f3e78217f6066875dbc868102f70b4f1149
Shaji <shajiphilip3022@gmail.com>
2024-10-14 01:14:24 -0400

    adding icon button

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -34,6 +34,7 @@ function ProductList() {
             return updatedState;
         });
     };
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -134,7 +135,7 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div><a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div>
+                    <div style={{ position: 'relative' }}>
                         <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                             <h1 className='cart'>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
@@ -143,8 +144,10 @@ function ProductList() {
                                     <circle cx="184" cy="216" r="12"></circle>
                                     <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
                                 </svg>
+                                <span className="cart_quantity_count">{totalQuantity}</span>
                             </h1>
                         </a>
+
                     </div>
                 </div>
             </div>
@@ -169,8 +172,7 @@ function ProductList() {
                                            disabled={!!addedToCart[plant.name]}  
                                             >
                                                  {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
-                                                
-                                                </button>
+                                            </button>
                                     </div>
                                 ))}
                             </div>
